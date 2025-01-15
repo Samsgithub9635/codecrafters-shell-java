cb9f8ad0a0a31aa0cb907e87e05122be7beef9b9
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-01-15 20:15:03 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
+        
+        
+        
 
-        Scanner scanner = new Scanner(System.in);
-        // In this stage, you'll implement support for handling invalid commands in your shell.
-        // $ invalid_command
-        // invalid_command: command not found
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while (true) {
+            // Print the prompt
+            System.out.print("$ ");
+            // In this stage, you'll implement support for handling invalid commands in your shell.
+            // $ invalid_command
+            // invalid_command: command not found
+            Scanner scanner = new Scanner(System.in);
+            // Read user input
+            String input = scanner.nextLine();
+            
+            // Handle invalid commands
+            System.out.println(input + ": command not found");
+        }
+        
+        
     }
 }
